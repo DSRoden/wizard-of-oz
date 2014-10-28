c862f6ba849c9ad0bbb2485b846115fcf898b224
Liza Carter <lizahcarter@gmail.com>
2014-10-28 15:13:57 -0500

    added start function to menu

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,3 +1,21 @@
 (function(){
   game.state.add('menu', {preload:preload, create:create});
+
+  function preload(){
+    game.load.tilemap('map', '/assets/catastrophi_level3.csv', null, Phaser.Tilemap.CSV);
+    game.load.image('tiles', '/assets/catastrophi_tiles_16.png');
+    game.load.spritesheet('player', '/assets/spaceman.png', 16, 16);
+  }
+
+  function create(){
+    var text = game.add.text(game.world.centerX, game.world.centerY, 'Press SPACE to Begin');
+    text.anchor.setTo(0.5);
+
+    var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(start);
+  }
+   function start(){
+    game.state.start('lvl1');
+  }
+
 })();
