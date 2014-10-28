87a8cc5353365e3d7fb07e005301b58f373ff64e
DSRoden <Daniel.S.Roden@gmail.com>
2014-10-28 13:43:14 -0500

    setup

---
+++ app.js
@@ -0,0 +1,12 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var morgan = require('morgan');
+var port = process.env.PORT;
+
+var app = connect();
+
+app.use(morgan('dev'));
+app.use(serveStatic('public'));
+
+app.listen(port);
+console.log('Node listening on port ' + port);
