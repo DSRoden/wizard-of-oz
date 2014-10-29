a1d16b66732bc113a89a28d793707bf1203924b1
DSRoden <Daniel.S.Roden@gmail.com>
2014-10-29 14:49:35 -0500

    world2 gameplay

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,12 +5,13 @@
     game.load.image('startscreen', '../assets/menu/startscreen.png');
     game.load.image('start', '../assets/menu/start-button.png');
 
-    game.load.tilemap('map', '../assets/catastrophi_level2.csv', null, Phaser.Tilemap.CSV);
+    game.load.tilemap('mapGold', '../assets/world2/oz_world.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.tilemap('mapBw', '../assets/world1/kansas.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', '../assets/catastrophi_tiles_16.png');
     game.load.image('twister', '../assets/world1/tornado.png');
     game.load.image('moneyBag', '../assets/world1/package.png');//moneyBag image
     game.load.image('kansas', '../assets/world1/tmw_desert_spacing_bw.png');
+    game.load.image('Oz', '../assets/world2/tmw_desert_spacing_gold.png');
     game.load.spritesheet('player', '../assets/wizard.png', 23, 30);
     game.load.audio('collectMoney', '../assets/audio/collectMoney.mp3');
     game.load.audio('twister', '../assets/audio/twister.wav');
